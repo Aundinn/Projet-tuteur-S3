2fd661ad1102c949a2b5f36f33d4f393bf6a47a3
BastienRebeyrat <bastien.rebeyrat@gmail.com>
2015-01-12 11:06:15 +0100

    Ajout des entités

---
+++ Groupe 4/Symfony/src/PR2L/SiteBundle/Entity/Entity/utilisateur.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace PR2L\SiteBundle\Entity\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * utilisateur
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="PR2L\SiteBundle\Entity\Entity\utilisateurRepository")
+ */
+class utilisateur
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="login", type="string", length=255)
+     */
+    private $login;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="droits", type="string", length=255)
+     */
+    private $droits;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     * @return utilisateur
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string 
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return utilisateur
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return utilisateur
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     * @return utilisateur
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string 
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set droits
+     *
+     * @param string $droits
+     * @return utilisateur
+     */
+    public function setDroits($droits)
+    {
+        $this->droits = $droits;
+
+        return $this;
+    }
+
+    /**
+     * Get droits
+     *
+     * @return string 
+     */
+    public function getDroits()
+    {
+        return $this->droits;
+    }
+}
