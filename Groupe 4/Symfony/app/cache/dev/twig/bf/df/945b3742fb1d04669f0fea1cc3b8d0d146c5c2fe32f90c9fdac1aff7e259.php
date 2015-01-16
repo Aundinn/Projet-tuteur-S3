0a96d4e33c7b13ecaaa3b335577ec851d647f8a0
Théo Lassaube <theo-alexis@club-internet.fr>
2015-01-16 08:10:58 +0100

    Simplification routing

---
+++ Groupe 4/Symfony/app/cache/dev/twig/bf/df/945b3742fb1d04669f0fea1cc3b8d0d146c5c2fe32f90c9fdac1aff7e259.php
@@ -0,0 +1,54 @@
+<?php
+
+/* PR2LSiteBundle:Default:gererPartenaires.html.twig */
+class __TwigTemplate_bfdf945b3742fb1d04669f0fea1cc3b8d0d146c5c2fe32f90c9fdac1aff7e259 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::layout.html.twig");
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = array())
+    {
+        $this->displayParentBlock("title", $context, $blocks);
+        echo " - Gérer les partenaires";
+    }
+
+    // line 5
+    public function block_body($context, array $blocks = array())
+    {
+    }
+
+    public function getTemplateName()
+    {
+        return "PR2LSiteBundle:Default:gererPartenaires.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  36 => 5,  29 => 3,);
+    }
+}
