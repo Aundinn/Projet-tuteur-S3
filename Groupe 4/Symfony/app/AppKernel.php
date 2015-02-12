76090e7fa2e88f4791ddb6c2ae65e0bc2f16d51d
Théo Lassaube <theo-alexis@club-internet.fr>
2015-02-12 14:15:12 +0100

    appkernel corrigé

--- Groupe 4/Symfony/app/AppKernel.php
+++ Groupe 4/Symfony/app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
             new PR2L\SiteBundle\PR2LSiteBundle(),
             new PR2L\UserBundle\PR2LUserBundle(),
             new PR2L\ArchivesBundle\PR2LArchivesBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
